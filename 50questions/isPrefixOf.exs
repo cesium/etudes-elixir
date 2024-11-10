d9b678222aa63180e5ee3667ed40368ccca43b19
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-11-10 15:59:27 +0000

    chore: removed 50questions

--- 50questions/isPrefixOf.exs
+++
@@ -1,8 +0,0 @@
-defmodule Questao do
-
-  #Implementa uma função que recebe duas listas e retorna true se a primeira for um prefixo da segunda.
-
-  def isPrefixOf(list1, list2) do
-    list1 == Enum.take(list2, length(list1)) #Enum.take pega os n (neste caso o tamanho de list1) elementos de uma lista
-  end
-end
